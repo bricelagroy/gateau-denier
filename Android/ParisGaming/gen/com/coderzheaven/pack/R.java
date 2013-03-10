2801f604ac8637753aa2ac583798a4dd0459b9f9
Brice Lagroy <brice.lagroy@fullsix.net>
2013-03-10 20:32:01 +0100

    change map and layout and add timer

--- Android/ParisGaming/gen/com/coderzheaven/pack/R.java
+++ Android/ParisGaming/gen/com/coderzheaven/pack/R.java
@@ -8,28 +8,28 @@
 package com.coderzheaven.pack;
 
 public final class R {
+    public static final class anim {
+        public static final int rotation=0x7f040000;
+    }
     public static final class attr {
     }
     public static final class drawable {
         public static final int button_custom=0x7f020000;
         public static final int carte_de_paris=0x7f020001;
-        public static final int icon=0x7f020002;
+        public static final int carte_paris=0x7f020002;
+        public static final int icon=0x7f020003;
     }
     public static final class id {
-        public static final int available_text=0x7f050001;
-        public static final int bouton=0x7f050005;
-        public static final int free_text=0x7f050002;
-        public static final int imageView1=0x7f050003;
-        public static final int placeName=0x7f050004;
-        public static final int title=0x7f050000;
+        public static final int imageView1=0x7f060001;
+        public static final int placeName=0x7f060000;
+        public static final int score=0x7f060002;
     }
     public static final class layout {
-        public static final int list_layout=0x7f030000;
-        public static final int main=0x7f030001;
+        public static final int main=0x7f030000;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int button=0x7f040002;
-        public static final int hello=0x7f040000;
+        public static final int app_name=0x7f050001;
+        public static final int button=0x7f050002;
+        public static final int hello=0x7f050000;
     }
 }
--- Android/ParisGaming/src/com/coderzheaven/pack/ParisGaming.java
+++ Android/ParisGaming/src/com/coderzheaven/pack/ParisGaming.java
@@ -2,9 +2,15 @@ package com.coderzheaven.pack;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import android.app.Activity;
+import android.os.CountDownTimer;
+import android.os.Handler;
 import android.view.MotionEvent;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.*;
 import android.os.Bundle;
 import android.view.View;
@@ -12,11 +18,12 @@ import android.view.View;
 public class ParisGaming extends Activity implements View.OnTouchListener  {
 
     private ImageView imageView;
-    private Button refreshButton;
+//    private Button refreshButton;
 
     private DataBaseHelper currentDbHelper;
 
     private Place currentPlace;
+    private Boolean _isImageToucheEnabled;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -24,14 +31,13 @@ public class ParisGaming extends Activity implements View.OnTouchListener  {
 
         setContentView(R.layout.main);
 
-        refreshButton = (Button) findViewById(R.id.bouton);
-        refreshButton.setOnTouchListener(this);
+//        refreshButton = (Button) findViewById(R.id.bouton);
+//        refreshButton.setOnTouchListener(this);
 
         imageView = (ImageView) findViewById(R.id.imageView1);
         imageView.setOnTouchListener(this);
-
         copyDB();
-
+        getNewPlace();
     }
 
     // Fonction qui sera lancée à chaque fois qu'un toucher est détecté sur le bouton rattaché
@@ -39,28 +45,39 @@ public class ParisGaming extends Activity implements View.OnTouchListener  {
     public boolean onTouch(View view, MotionEvent event) {
         if (event.getAction() == MotionEvent.ACTION_DOWN)
         {
-            if(view.equals(imageView))
+            if(view.equals(imageView) && _isImageToucheEnabled)
             {
                  if(currentPlace != null)
                  {
+                     _isImageToucheEnabled = false;
                      float goodX = imageView.getWidth() * currentPlace.map_x;
                      float goodY = imageView.getHeight() * currentPlace.map_y;
                      float currentX = event.getX();
                      float currentY = event.getY();
                      double finalement = Math.sqrt(Math.pow(goodX - currentX, 2) + Math.pow(goodY - currentY, 2));
-                     TextView t = (TextView) findViewById(R.id.placeName);
+                     TextView t = (TextView) findViewById(R.id.score);
                      t.setText("distance : " + finalement);
+                     startTimer();
                  }
             }
-            else if(view.equals(refreshButton))
-            {
-                currentPlace = currentDbHelper.getRandomPlace();
-                TextView t = (TextView) findViewById(R.id.placeName);
-                t.setText("Ou se trouve " + currentPlace.name + " ?");
-               // showthesqllove();
-            }
+//            else if(view.equals(refreshButton))
+//            {
+//                getNewPlace();
+//            }
         }
         return true;
+    }
+
+    private void startTimer(){
+        MyCount counter = new MyCount(5000,1000);
+        counter.start();
+    }
+
+    private void getNewPlace(){
+        currentPlace = currentDbHelper.getRandomPlace();
+        TextView t = (TextView) findViewById(R.id.placeName);
+        t.setText("Ou se trouve " + currentPlace.name + " ?");
+        _isImageToucheEnabled = true;
     }
 
     private void copyDB()
@@ -74,16 +91,34 @@ public class ParisGaming extends Activity implements View.OnTouchListener  {
         } catch (IOException ioe) {
 
             throw new Error("Unable to create database");
-
         }
         try {
-
             currentDbHelper.openDataBase();
-
         }catch(SQLException sqle){
 
             throw new Error("Unable to create database sql exception");
 
+        }
+    }
+
+    private void rotate()
+    {
+        Animation rotation = AnimationUtils.loadAnimation(this, R.anim.rotation);
+        imageView.startAnimation(rotation);
+    }
+
+    //countdowntimer is an abstract class, so extend it and fill in methods
+    public class MyCount extends CountDownTimer{
+        public MyCount(long millisInFuture, long countDownInterval) {
+            super(millisInFuture, countDownInterval);
+        }
+        @Override
+        public void onFinish() {
+            getNewPlace();
+        }
+
+        @Override
+        public void onTick(long millisUntilFinished) {
         }
     }
 }
